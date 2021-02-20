4711f0399e9317bcf4bbd3e8b8479fc67fe762b3
shunki-akiyama <brand.new.beat.4.22@gmail.com>
2021-02-20 12:47:47 -0800

    'Happiness-Quality_of_Life'

--- Happiness-Quality_of_Life.R
+++ Happiness-Quality_of_Life.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(readxl)
+library(tools)
 
 ####Load data####
 data <- read_excel("WHR20_DataForFigure2.1.xls")
@@ -57,6 +58,7 @@ data_ql_co$Country_co[data_ql_co$Country_co=="Central Africa"] <-"Central Africa
 #merge
 data <- left_join(data,data_ql_co, by =c("Country name"="Country_co"))
 summary(data)
+remove(data_ql_co)
 
 #delete 6 rows from data
 data <- data[complete.cases(data),]
@@ -69,11 +71,11 @@ data_qul_city <- read.csv("uaScoresDataFrame.csv", header=T)
 newcountry <- read.csv("newcountryname.csv", header=T)
 colnames(data_qul_city)
 colnames(newcountry)
-summary(newqualreport)
-head(newqualreport)
+summary(data_qul_city)
+head(data_qul_city)
 
 # change the column name
-names(newcountry)[names(newcountry) == "ï..UA_newcountry"] <- "UA_newcountry"
+names(newcountry)[names(newcountry) == "?..UA_newcountry"] <- "UA_newcountry"
 colnames(newcountry)
 
 
@@ -83,6 +85,7 @@ colnames(newcountry)
 
 data_qul_city2 <- data.frame(data_qul_city,newcountry)
 colnames(data_qul_city2)
+remove(newcountry)
 
 # remove column 1,3 (X rage and UA_Country)
 data_qul_city2 <- data_qul_city2[,-c(1,3)] 
@@ -119,7 +122,6 @@ colnames(data_qul_city3)
 summary(data_qul_city3)
 remove(data_qul_city)
 remove(data_qul_city2)
-remove(data_qul_city3)
 
 # check merge result
 testdata <- data
@@ -135,7 +137,8 @@ remove(testdata)
 data <- left_join(data,data_qul_city3, 
                   by=c("Country name"="UA_newcountry_city"))
 summary(data)
-data$`Country name`[is.na(data$Outdoors_city)]  #there are 58 NA observations
+sort(data$`Country name`[is.na(data$Outdoors_city)])  #there are 58 NA observations
+remove(data_qul_city3)
 
 #delete NA observations (147-58 = 89 observations remain)
 data <- data[complete.cases(data),]
@@ -145,10 +148,31 @@ summary(data)
 write.csv(data,file="testdata1.csv", row.names = F)
 
 
+#### Merge Suicide ####
+# open the file & change column name
+data_sui <-  read.csv("Suicide rates.csv", header=T)
+head(data_sui)
+data_sui <- data_sui[,c(1,3)]
+names(data_sui) =c("country", "suicide")
+
+#Convert country To Title Case
+data_sui$country <- toTitleCase(data_sui$country)
+
+#check_merge result
+check_merge <- left_join(data,data_sui, by =c("Country name"="country"))
+summary(check_merge)
+check_merge$`Country name`[is.na(check_merge$Popularity_co)]
+remove(check_merge)
+
+#merge 'Suicide' to 'Data'
+data <- left_join(data,data_sui, by =c("Country name"="country"))
+summary(data)
+remove(data_sui)
+
 ####changing data types####
 summary(data)
 data$Region <- factor(data$Region)
 data$Score_cat <- factor(data$Score_cat, levels= c("Low", "Middle", "High")) 
 
 #### check correlation####
-cor(data[,c(3:9,11:17)])
+cor(data[,c(3:9,11:35)])
